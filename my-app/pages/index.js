20a458b8649e9f92112daedf07b774fef6e92ec2
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-01-18 21:28:03 +0300

    build-storybook

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import Button from 'components/button'
+import Navigation from "components/navigation";
 
 function HomePage() {
-  return <div>
-    <h1>Welcome to Next.js!</h1>
-
-    <Button>Merhaba</Button>
-
-
-  </div>
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
