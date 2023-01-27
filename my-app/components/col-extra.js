cb2e835c87b16e5974747db5f6145163cf1c158f
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-01-27 11:08:11 +0300

    Update col-extra.js

--- my-app/components/col-extra.js
+++ my-app/components/col-extra.js
@@ -11,7 +11,8 @@ function Extra({ children }) {
 	
   // geçici
 	return <div>
-  			<div class="search-bar">
+  			<aside class="right-side">
+			<div class="search-bar">
 				<svg viewBox="0 0 24 24" aria-hidden="true">
 					<g>
 						<path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
@@ -182,6 +183,7 @@ function Extra({ children }) {
 			<span>© 2023 Twitter, Inc.</span>
 			</nav>
 			</footer>
+		</aside>
   
   // geçici
   </div>;
