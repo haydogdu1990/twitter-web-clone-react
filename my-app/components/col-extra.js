d99920a73cc1a803d56ec468ee2cc3c1ed5bb188
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-01-27 16:02:09 +0300

    Update col-extra.js

--- my-app/components/col-extra.js
+++ my-app/components/col-extra.js
@@ -1,14 +1,12 @@
 import React from "react";
 import cn from "classnames";
-import GeciciExtra from "./gecici-extra";
+
 import styles from "./col-extra.module.css";
 
 
 
 function Extra({ children }) {
-  return <div className={cn(styles.extra)}>{children}
-    <GeciciExtra/>
-  </div>;
+  return <div className={cn(styles.extra)}>{children}</div>;
 }
 
 export default Extra;
